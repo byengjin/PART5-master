934a63dd79f80df78e300cced713dea3a69edd21
byeongjin <cbj9988@naver.com>
2024-11-07 12:50:13 +0900

    연습용

---
+++ src/test/java/org/zerock/b01/repository/ReplyRepositoryTests.java
@@ -0,0 +1,56 @@
+package org.zerock.b01.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.b01.domain.Board;
+import org.zerock.b01.domain.Reply;
+import org.zerock.b01.repository.search.ReplyRepository;
+
+import javax.transaction.Transactional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepositoryTests {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+
+        Long bno = 100L;
+
+        Board board = Board.builder().bno(bno).build();
+
+        Reply reply = Reply.builder()
+                .board(board)
+                .replyText("1234")
+                .replyer("replyer1")
+                .build();
+
+        replyRepository.save(reply);
+        });
+    }
+    @Transactional
+    @Test
+    public void testBoardReplies() {
+
+        Long bno = 100L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listofBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
+    }
+}
